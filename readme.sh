437615e66025cc8819cee33dd4dad687e5d2181b
oudream <oudream@126.com>
2020-05-14 15:51:08 +0800

    Update readme.sh

--- readme.sh
+++ readme.sh
@@ -17,3 +17,7 @@ drive.mount("/content/drive")
 !pip install torch torchvision yacs tqdm opencv-python vizer tensorboardX
 
 !cd /content/drive/'My Drive'/colab/hello-ssd1 && python train.py --config-file configs/vgg_ssd300_voc0712.yaml
+
+!cd /content/drive/'My Drive'/colab/hello-ssd1 && git pull origin master
+
+!cd /content/drive/'My Drive'/colab/hello-ssd1 && python demo.py --config-file configs/vgg_ssd300_voc0712.yaml --images_dir demo --score_threshold 0.7 --ckpt ./outputs/vgg_ssd300_voc0712/model_final.pth
